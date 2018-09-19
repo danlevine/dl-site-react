74547a7788ffbf7f948a36148e943d75947654a1
Daniel Levine <dlevine210@gmail.com>
2018-09-19 13:51:45 -0500

    Move App.jsx styles into JSX file

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,31 +1,64 @@
 import React, { Component } from "react";
 import { Router, Link } from "@reach/router";
+import styled from "styled-components";
 import ContentAbout from "../ContentAbout";
 import ContentHome from "../ContentHome";
 import ContentLinks from "../ContentLinks";
 
 import logo from "../../assets/logo.svg";
-import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <AppStyled className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <Link to="/">home</Link>
+          <Link to="/about">about</Link>
+          <Link to="/links">links</Link>
         </header>
-        {/* <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
         <Router>
           <ContentHome path="/" />
           <ContentAbout path="/about" />
           <ContentLinks path="/links" />
         </Router>
-      </div>
+      </AppStyled>
     );
   }
 }
+
+const AppStyled = styled.div`
+  text-align: center;
+
+  .App-logo {
+    animation: App-logo-spin infinite 20s linear;
+    height: 80px;
+  }
+
+  .App-header {
+    background-color: #222;
+    height: 150px;
+    padding: 20px;
+    color: white;
+  }
+
+  .App-title {
+    font-size: 1.5em;
+  }
+
+  .App-intro {
+    font-size: large;
+  }
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
 
 export default App;
